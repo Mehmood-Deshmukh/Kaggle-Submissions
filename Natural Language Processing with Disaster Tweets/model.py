bc3c1dcf651b0ce9d38d50a7d307769b24352d7b
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-06-27 16:49:55 +0530

    nlp- uploaded python file

---
+++ Natural Language Processing with Disaster Tweets/model.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+stop_words.remove('not')
+
+dataset = pd.read_csv('train.csv')
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r'<.*?>+', '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
+    text = ' '.join(word for word in text.split() if word not in stop_words)
+    return text
+
+dataset['cleaned_text'] = dataset['text'].apply(clean_text)
+dataset['keyword'].fillna('', inplace=True)
+dataset['location'].fillna('', inplace=True)
+
+dataset['combined_text'] = dataset['cleaned_text'] + ' ' + dataset['keyword'] + ' ' + dataset['location']
+
+vectorizer = TfidfVectorizer(max_features=5000)
+X = vectorizer.fit_transform(dataset['combined_text'])
+y = dataset['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=1000, random_state=42)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
+print("accuracy: ", accuracy_score(y_test, y_pred))
+
+test_data = pd.read_csv('test.csv')
+test_data['cleaned_text'] = test_data['text'].apply(clean_text)
+test_data['keyword'].fillna('', inplace=True)
+test_data['location'].fillna('', inplace=True)
+test_data['combined_text'] = test_data['cleaned_text'] + ' ' + test_data['keyword'] + ' ' + test_data['location']
+X_test = vectorizer.transform(test_data['combined_text'])
+y_pred = classifier.predict(X_test)
+
+submission = pd.DataFrame({'id': test_data['id'], 'target': y_pred})
+submission.to_csv('submission.csv', index=False)
